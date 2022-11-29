e4f50c5c7596c4972e11215c7855d8393aae7225
mrinal975 <mrinalmallik1@gmail.com>
2022-11-29 06:49:05 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Headers';
 
 function App() {
   return (
-    <div className="w-screen">
-      <div class="flex flex-row">
-        <div class="basis-1/4">01</div>
-        <div class="basis-1/4">02</div>
-        <div class="basis-2/4">03</div>
-      </div>
+    <div className="w-screen h-auto flex flex-col">
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Headers.jsx
@@ -0,0 +1,9 @@
+const Header =()=>{
+    return (
+        <div className="fixed z-50 w-screen bg-slate-300 p-6 px-60">
+            This is Header
+        </div>
+    )
+}
+
+export default Header;
