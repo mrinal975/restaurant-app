85aba916a5c546648eedb9435c9b088496da8b5d
mrinal975 <mrinalmallik1@gmail.com>
2022-11-29 17:08:41 +0600

    header item icon &  menu

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,7 +1,29 @@
+import React from "react";
+import Logo from "./img/logo.png";
 const Header =()=>{
     return (
-        <div className="fixed z-50 w-screen bg-slate-300 p-6 px-60">
-            This is Header
+        <div className="fixed z-50 w-screen p-5 px-20">
+            <div className="hidden md:flex w-full h-full">
+                <div className="flex items-center gap-2">
+                    <img src={Logo} className="w-10 object-cover" alt="logo"/>
+                    <p className="text-headingColor text-xl font-bold">City</p>     
+                </div>
+                <ul className="flex items-center gap-8 ml-auto">
+                    <li className="text-base text-textColor hover:text-headingColor
+                    duration-00 transition-all ease-in-out cursor-pointer">
+                        Home
+                    </li>
+                    <li className="text-base text-textColor hover:text-headingColor
+                    duration-00 transition-all ease-in-out cursor-pointer">
+                        Menu
+                    </li>
+                    <li className="text-base text-textColor hover:text-headingColor
+                    duration-00 transition-all ease-in-out cursor-pointer">
+                        About Us
+                    </li>
+                </ul>
+            </div>
+            <div className="flex md:hidden w-full h-full"></div>
         </div>
     )
 }
