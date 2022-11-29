057cb4fbaa41e8e9f0101f43e37112ad6328b58d
mrinal975 <mrinalmallik1@gmail.com>
2022-11-29 06:06:59 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-screen">
+      <div class="flex flex-row">
+        <div class="basis-1/4">01</div>
+        <div class="basis-1/4">02</div>
+        <div class="basis-2/4">03</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
