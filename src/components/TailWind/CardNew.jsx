791c86955b423322c504c611de7cd81ac6e7f038
mrinal975 <mrinalmallik1@gmail.com>
2022-12-03 21:51:11 +0600

    flex

--- src/components/TailWind/CardNew.jsx
+++ src/components/TailWind/CardNew.jsx
@@ -3,6 +3,35 @@ import React from "react";
 const CardNew =()=>{
     return (
         <>
+        <div className="max-w-[1320px] md:py-[80] py-5 flex mx-auto sm:flex-row flex-col">
+                <div className="basis-[45%]">
+                    <img src="https://thumbs.dreamstime.com/z/student-girl-studying-school-education-concept-little-reading-books-37772013.jpg"/>
+                </div>
+                <div className="basis-[55%] px-5">
+                    <h1 className="text-4xl pd-5">Information technology (IT)</h1>
+                    <p className="py-3">
+                        The term is commonly used as a synonym for computers and computer networks, 
+                        but it also encompasses other information distribution technologies such as television and telephones.
+                        Several products or services within an economy are associated with information technology
+                    </p>
+                    <p className="py-3">
+                        The term is commonly used as a synonym for computers and computer networks, 
+                        but it also encompasses other information distribution technologies such as television and telephones.
+                        Several products or services within an economy are associated with information technology
+                    </p>
+                    <p className="py-3">
+                        The term is commonly used as a synonym for computers and computer networks, 
+                        but it also encompasses other information distribution technologies such as television and telephones.
+                        Several products or services within an economy are associated with information technology
+                    </p>
+                    <p className="py-3">
+                        The term is commonly used as a synonym for computers and computer networks, 
+                        but it also encompasses other information distribution technologies such as television and telephones.
+                        Several products or services within an economy are associated with information technology
+                    </p>
+
+                </div>
+            </div>
         <div className="max-w-[1320px] mx-auto">
             <h1 className="text-4xl text-center py-5">Feature Products</h1>
         </div>
