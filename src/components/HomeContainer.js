2f159898f555c7f37bc5e2a26c1420416590770b
mrinal975 <mrinalmallik1@gmail.com>
2022-12-08 21:48:51 +0600

    rename file name

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Delivery from "../img/delivery.png";
+import HeroBg from "../img/heroBg.png";
 
-const MainContainer = () => {
+const HomeContainer = () => {
   return (
-    <div className="grid grid-clos-1 md:grid-cols-2 gap-2  ">
-      <div className="py-2 flex-1 flex flex-col items-center md:items-start justify-center">
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full h-screen">
+      <div
+        className="py-2 flex-1 flex flex-col
+       items-center md:items-start justify-center gap-6"
+      >
         <div
           className="flex item-started gap-2 justify-center
-         bg-orange-100 p-2 rounded-full"
+     bg-orange-100 p-2 rounded-full"
         >
           <p>Bike Delivery</p>
           <div className="w-6 h-6 rounded-full bg-white overflow-hidden">
@@ -30,16 +34,19 @@ const MainContainer = () => {
         <button
           type="button"
           className="bg-gradient-to-br from-orange-400 to-orange-400
-        w-full px-4 py-2 rounded-lg hover:sha translate-all ease-in-out duration-100
-        md:w-auto
-        "
+            w-full px-4 py-2 rounded-lg hover:sha translate-all ease-in-out duration-100
+            md:w-auto"
         >
           Order now
         </button>
       </div>
-      <div className="py-2 bg-blue-400 flex-1"></div>
-    </div>
+      <div className="py-2  flex-1">
+        <div className="w-full flex items-center justify-center relative">
+          <img src={HeroBg} alt="hero bg" />
+        </div>
+      </div>
+    </section>
   );
 };
 
-export default MainContainer;
+export default HomeContainer;
---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeContainer from "./HomeContainer";
+
+const MainContainer = () => {
+  return (
+    <div className="w-full h-auto flex flex-auto items-center justify-center">
+      <HomeContainer />
+    </div>
+  );
+};
+
+export default MainContainer;
