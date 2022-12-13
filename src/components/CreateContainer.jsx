7b18e966b9a174ecb50f8621546541d280e15d8f
mrinal975 <mrinalmallik1@gmail.com>
2022-12-13 11:30:28 +0600

    loader added

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-
-import {
-  MdFastfood
-} from "react-icons/md";
+import { MdFastfood, MdCloudUpload } from "react-icons/md";
 import { categories } from "../utils/data";
+import Leading from "./Leading";
+
 const CreateContainer = () => {
   const [title, setTitle] = useState("");
   const [calories, setCalories] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState(null);
-  const [imageAsset, setImageAsset] = useState(null);
+  const [imageAsset, setImageAsset] = useState(true);
   const [fields, setFields] = useState(false);
   const [alertStatus, setAlertStatus] = useState("danger");
   const [msg, setMsg] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const uploadImage = () => {};
 
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
@@ -41,14 +43,16 @@ const CreateContainer = () => {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             placeholder="Give me a title..."
-            className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
+            className="w-full h-full text-lg bg-transparent outline-none border-none 
+            placeholder:text-gray-400 text-textColor"
           />
         </div>
 
         <div className="w-full">
           <select
             onChange={(e) => setCategory(e.target.value)}
-            className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+            className="outline-none w-full text-base border-b-2 border-gray-200 p-2
+             rounded-md cursor-pointer"
           >
             <option value="other" className="bg-white">
               Select Category
@@ -66,6 +70,40 @@ const CreateContainer = () => {
           </select>
         </div>
 
+        <div
+          className="group flex justify-cente items-center flex-col
+        border-2 border-dotted border-gray-300 w-full h-225 md:h-420
+        cursor-pointer roun></Leadinged-lg"
+        >
+          {isLoading ? <Leading /> : <></>}
+          {imageAsset ? (
+            <>
+              <label
+                className="w-full h-full flex flex-col items-center
+              justify-center cursor-pointer "
+              >
+                <div
+                  className="w-full h-full flex flex-col
+                 items-center justify-center gap-2"
+                >
+                  <MdCloudUpload className="ext-gray-500 text-3xl hover:text-gray-700" />
+                  <p className="text-gray-500 hover:text-gray-700">
+                    Click here to upload
+                  </p>
+                </div>
+                <input
+                  type="file"
+                  name="uploadimage"
+                  accept="image/*"
+                  onChange={uploadImage}
+                  className="w-0 h-0"
+                />
+              </label>
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </div>
   );
