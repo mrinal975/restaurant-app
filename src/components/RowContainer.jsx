501fb49bf605bd174a7d517369f1752a76f6e9e5
mrinal975 <mrinalmallik1@gmail.com>
2022-12-14 12:23:21 +0600

    row container design

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,10 +1,42 @@
+import { MdShoppingBasket } from "react-icons/md";
+import { motion } from "framer-motion";
+
 const RowContainer = ({ flag }) => {
   return (
     <div
-      className={`w-full my-12  bg-black ${
+      className={`w-full my-12  bg-cardOverlay ${
         flag ? "overflow-x-scroll" : "overflow-x-hidden"
       }`}
-    ></div>
+    >
+      <div className="w-300 md:w-340 bg-gray-100 h-auth backdrop-blur-lg p-2 my-2 hover:drop-shadow-2xl">
+        <div className="w-full flex items-center justify-between">
+          <motion.img
+            whileHover={{ scale: 1.2 }}
+            src="https://firebasestorage.googleapis.com/v0/b/restaurant-app-7726c.appspot.com/o/Images%2F1670952111564-download.jpeg?alt=media&token=5a3b63e7-883e-41da-b510-44d0cf029a09"
+            alt="image"
+            className="w-44 -mt-8 drop-shadow-2xl"
+          />
+          <motion.div
+            whileTap={{ scale: 0.75 }}
+            className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md"
+          >
+            <MdShoppingBasket className="text-white" />
+          </motion.div>
+        </div>
+        <div className="w-full flex flex-col items-end justify-end">
+          <p className="text-textColor font-semibold text-base md:text-lg">
+            Chocolate & Vanela
+          </p>
+          <p className="mt-1 text-sm text-gray-500">4 calories</p>
+          <div className="flex items-center gap-8">
+            <p className="text-lg text-headingColor font-samibold">
+              <span className="text-sm text-red-500">$</span>
+              5.25
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,41 +3,43 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      width:{
-        150:"150px",
-        190:"190px",
-        225:"225px",
-        275:"275px",
-        300:"3300px",
-        340:"340px",
-        350:"350px",
+      width: {
+        150: "150px",
+        190: "190px",
+        225: "225px",
+        275: "275px",
+        300: "3300px",
+        340: "340px",
+        350: "350px",
       },
-      height:{
-        80:"80px",
-        150:"150px",
-        225:"225px",
-        300:"300px",
-        340:"34px",
+      height: {
+        80: "80px",
+        150: "150px",
+        225: "225px",
+        300: "300px",
+        340: "34px",
       },
-      minWidth:{
-        210:"210px",
-        350:"350px",
-        620:"620px",
+      minWidth: {
+        210: "210px",
+        350: "350px",
+        620: "620px",
       },
-      screens:{
-        sm:"640px",
-        md:"768px",
-        lg:"1024px",
-        xl:"1280px",
-        "2xl":"1536px",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
-      colors:{
-        headingColor:"#2e2e2e",
-        textColor:"#51551",
-        cartNumBg:"#e80013",
-        primary:"#f5f3f3"
-      }
+      colors: {
+        headingColor: "#2e2e2e",
+        textColor: "#51551",
+        cartNumBg: "#e80013",
+        primary: "#f5f3f3",
+        rowBg: "rgba(255,131,0,0.1)",
+        cardOverlay: "rgba(256,256,256,0.4)",
+      },
     },
   },
   plugins: [],
-}
+};
