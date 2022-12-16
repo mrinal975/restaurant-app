a3ad0eb189baa9d2ad65bd86a707bb8dadd1847d
mrinal975 <mrinalmallik1@gmail.com>
2022-12-16 23:18:29 +0600

    card item design added

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { MdOutlineKeyboardBackspace } from "react-icons/md";
 import { motion } from "framer-motion";
 import { RiRefreshFill } from "react-icons/ri";
+import { BiMinus, BiPlus } from "react-icons/bi";
+
 const CartContainer = () => {
   return (
     <div className="fixed top-0 right-0 w-full md:w-375 bg-white drop-shadow-md flex flex-col z-[101]">
@@ -18,8 +20,9 @@ const CartContainer = () => {
           {""}
         </motion.p>
       </div>
-      {/* button section */}
+      {/* bottom section */}
       <div className="w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col">
+        {/* cart item section */}
         <div className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
           {/* cart item */}
           <div className="w-full p-1 px-2 rounded-lg bg-cartItem flex items-center gap-2">
@@ -40,14 +43,41 @@ const CartContainer = () => {
             {/* button section */}
             <div className="group flex items-center gap-2 ml-auto cursor-pointer">
               <motion.div whileTap={{ scale: 0.75 }}>
-                {/* <BiMinus className="text-gray-50" /> */}
+                <BiMinus className="text-gray-50" />
               </motion.div>
-              <p className="w-5 h-5 rounded-sm bg-cartBg text-gray-50 flex items-center justify-center"></p>
+              <p className="w-5 h-5 rounded-sm bg-cartBg text-gray-50 flex items-center justify-center">
+                1
+              </p>
               <motion.div whileTap={{ scale: 0.75 }}>
-                {/* <BiPlus className="text-gray-50" /> */}
+                <BiPlus className="text-gray-50" />
               </motion.div>
             </div>
           </div>
+        </div>
+        {/* cart total section */}
+        <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
+          <div className="w-full flex items-center justify-between">
+            <p className="text-gray-400 text-lg">Sub Total</p>
+            <p className="text-gray-400 text-lg">$ 8.5</p>
+          </div>
+          <div className="w-full flex items-center justify-between">
+            <p className="text-gray-400 text-lg">Delivery</p>
+            <p className="text-gray-400 text-lg">$ 2.5</p>
+          </div>
+
+          <div className="w-full border-b border-gray-600 my-2"></div>
+
+          <div className="w-full flex items-center justify-center">
+            <p className="text-gray-400 text-lg">Total</p>
+            <p className="text-gray-400 text-lg">$ 2.5</p>
+          </div>
+          <motion.button
+            whileTap={{ scale: 0.8 }}
+            type="button"
+            className="w-full p-2 rounded-full bg-yellow-600 text-gray-50 text-lg my-2 hover:shadow-lg transition-all duration-150 ease-out"
+          >
+            Check out
+          </motion.button>
         </div>
       </div>
     </div>
